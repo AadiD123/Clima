2a415b0e197a717d7f958e948c71b391d03d1414
Aaditya Doiphode <aadid2004@gmail.com>
2021-01-04 23:07:40 -0800

    Initial commit

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Aaditya Doiphode on 11/23/19.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let id: Int
+}
+
+
